dddbdd23161a86147310245d0fe3a076d8da9e7f
Andre Lima <andresdslima@gmail.com>
2022-04-11 14:29:59 +0200

    fixing bugs

--- js/components/ListItem.js
+++
@@ -1,8 +0,0 @@
-const ListItem = Vue.component('listItem', {
-    props: ['name', 'abv'],
-    template: `
-        <li v-for="(item, index) in data" class="list-group-item">
-            {{ index + 1 }}. {{ item.name }}; ABV: {{ item.abv}}.
-        </li>
-    `
-});
